5302f4799484dae1f70d04bc4a6398c8d0f0e2bc
MigenaGega1 <migena.gega10@gmail.com>
2022-11-01 12:10:01 +0100

    last

--- app/Http/Controllers/TravelCostController.php
+++ app/Http/Controllers/TravelCostController.php
@@ -15,24 +15,29 @@ class TravelCostController extends Controller
     public function calculate(Request $request)
     {
 
+
         $distanca = $request->distance;
+
         //Skenari A
         //Cmimi per kilometer eshte 5 Euro. Nese udhetimi kalon mbi 100 kilometra cmimi per kilometrat e tjera do te jete 3 Euro per kilometer.
         //Cmimi per ore eshte 4 Euro per nje ore udhetimi.
 
-        if ($distanca > 100) {
-            $pricekm = ($distanca - 100) * 3 + (100 * 5);
-        } else {
-            $pricekm = $distanca * 5;
-        }
-        $priceForMinutes = 4 / 60;
+//        if ($distanca > 100) {
+//            $pricekm = ($distanca - 100) * 3 + (100 * 5);
+//        } else {
+//            $pricekm = $distanca * 5;
+//        }
+//        $priceForMinutes = 4 / 60;
         $startedAt = Carbon::createFromFormat('Y-m-d\TH:i', $request->start_time);
         $endedAt = Carbon::createFromFormat('Y-m-d\TH:i', $request->end_time);
-        $diff = $endedAt->diffInMinutes($startedAt);
-        $timepriceA = $diff * $priceForMinutes;
+        dd(self::calculateB($startedAt, $endedAt, $distanca));
+//        $diff = $endedAt->diffInMinutes($startedAt);
 
-        $totalprice = $pricekm + $timepriceA;
-        dd($totalprice, self::calculateB($startedAt, $endedAt, $distanca));
+//        $timepriceA = $diff * $priceForMinutes;
+//        $totalprice = $pricekm + $timepriceA;
+        $price = 0;
+        $pricekm = $distanca * 3;
+        $priceTime = 0;
 
 
         return view('travelCost');
@@ -47,36 +52,31 @@ class TravelCostController extends Controller
         // Nga ora 00:00 deri ne oren 06:00 cmimi eshte 12 Euro, jashte kesaj fashe orare cmimi per ore eshte 7 Euro.
         //Nese cmimi kalon mbi 70 Euro atehere per qe nga momenti i nisjes se udhetimit per 24 oret e ardheshme udhetimi do te jete i mbuluar nga ky cmim.
 
-        $totalminutes = 0;
-        $minutesoutSlot = 0;
-        $minutesInSlot = 0;
+
         $priceKm = $distanca * 3;
-        for ($d = $d1; $d < $d2; $d->addMinute()) {
-            $totalminutes++;
-            $s = explode(" ", $d);
+        $flag = 0;
+        $price = 0;
+        $start = $d1->copy();
+        while ($d1 <= $d2) {
+            $s = explode(" ", $d1);
             $from = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '00:00:00');
             $to = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '06:00:00');
 
-            if ($d >= $from && $d < $to) {
-                $minutesInSlot++;
+            if ($d1 >= $from && $d1 < $to) {
+                $price += 12 / 60;
+
             } else {
-                $minutesoutSlot++;
+                $price += 7 / 60;
+
+            }
+            $d1 = $d1->addMinute();
+            if ($price >= 70 && $flag == 0) {
+                $d1 = $start->addDay();
+                $flag = 1;
+
             }
         }
-
-        $priceInSlot = $minutesInSlot * 12 / 60;
-        $priceOutSlot = $minutesoutSlot * 7 / 60;
-        $priceByTime = $priceInSlot + $priceOutSlot;
-        if ($priceByTime > 70 && $totalminutes >= 24*60) {
-            $priceByTime = $priceByTime - (6 * 12 + 18 * 7) + 70;
-
-        } else if ($priceByTime > 70 && $totalminutes < 24*60) {
-            $priceByTime = 70;
-
-        } else {
-            $priceByTime = $minutesInSlot * 12/60 + $minutesoutSlot * 7/60;
-        }
-        return $priceKm + $priceByTime;
+        return $price;
     }
 
 
@@ -95,32 +95,28 @@ class TravelCostController extends Controller
         if ($distanca > 100) {
             $priceKm = ($distanca - 100) * 3 + (50 * 5) + (50 * 7);
         }
-        $totalminutes = 0;
-        $minutesoutSlot = 0;
-        $minutesInSlot1 = 0;
-        $minutesInSlot2 = 0;
-        $minutesInSlot3 = 0;
-        for ($c = $d1; $c < $d2; $c->addMinute()) {
-            $totalminutes++;
-            $s = explode(" ", $c);
+
+       $price=0;
+        while($d1<=$d2){
+            $s = explode(" ", $d1);
             $t1 = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '00:00:00');
             $t2 = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '07:00:00');
             $t3 = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '09:00:00');
             $t4 = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '16:00:00');
             $t5 = Carbon::createFromFormat('Y-m-d H:i:s', $s[0] . ' ' . '18:30:00');
-            if ($c >= $t1 && $c < $t2) {
-                $minutesInSlot1++;
-            } elseif ($c >= $t2 && $c < $t3) {
-                $minutesInSlot2++;
-            } elseif ($c >= $t4 && $c < $t5) {
-                $minutesInSlot3++;
+            if ($d1 >= $t1 && $d1 < $t2) {
+                $price += 10 / 60;
+            } elseif ($d1 >= $t2 && $d1 < $t3) {
+                $price += 8 / 60;
+            } elseif ($d1 >= $t4 && $d1 < $t5) {
+                $price += 8 / 60;
             } else {
-                $minutesoutSlot++;
+                $price += 5 / 60;
             }
+            $d1 = $d1->addMinute();
         }
-        $timePrice = $minutesInSlot1 * 10 / 60 + $minutesInSlot2 * 8 / 60 + $minutesInSlot3 * 8 / 60 + $minutesoutSlot * 5 / 60;;
-        return $timePrice + $priceKm;
-    }
 
+        return $price;
+    }
 
 }
